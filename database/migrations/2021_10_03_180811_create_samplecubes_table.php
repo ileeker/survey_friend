b6c2fc179ddfad0fd72f890a9fd82207af61886b
ileeker <leeker@live.com>
2021-10-04 07:04:44 +0800

    Update 2021_10_03_180811_create_samplecubes_table.php

--- database/migrations/2021_10_03_180811_create_samplecubes_table.php
+++ database/migrations/2021_10_03_180811_create_samplecubes_table.php
@@ -16,7 +16,7 @@ class CreateSamplecubesTable extends Migration
         Schema::create('samplecubes', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('surveyid')->nullable();
-            $table->integer('groupid')->nullable();
+            $table->string('groupid')->nullable();
             $table->string('surveyname')->nullable();
             $table->integer('totalquota')->nullable();
             $table->integer('remainquota')->nullable();
