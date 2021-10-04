154c43607c24141ff538150d6dcc21bcebc24406
ileeker <leeker@live.com>
2021-10-04 11:08:07 +0800

    7

--- app/Console/Commands/ScGroupUpdate.php
+++ app/Console/Commands/ScGroupUpdate.php
@@ -3,8 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Survey\Scgroup;
-use App\Survey\Samplecube;
+use App\Scgroup;
+use App\Samplecube;
 
 class ScGroupUpdate extends Command
 {
